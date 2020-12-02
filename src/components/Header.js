cbd1814e058f9a4c75c5379d7c34288e097325f5
pashaapsky <ap.sky@yandex.ru>
2020-12-02 20:04:58 +0300

    start create youtube proj

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../scss/header.scss"
+
+function Header(props) {
+    return (
+        <div className="header">
+
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/configs/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCQ85NysIlfqGprzkgcmtoDpqx2TMbPU9g",
+    authDomain: "react-youtub3-clone.firebaseapp.com",
+    databaseURL: "https://react-youtub3-clone.firebaseio.com",
+    projectId: "react-youtub3-clone",
+    storageBucket: "react-youtub3-clone.appspot.com",
+    messagingSenderId: "582366171766",
+    appId: "1:582366171766:web:7f7760e2f30eb04dc3c557"
+};
